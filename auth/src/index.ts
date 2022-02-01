78bfb98d7240b31cb9951a2383055b1d3434e389
Rares Jianu <Rares.Jianu@endava.com>
2022-02-01 20:03:36 +0200

    Add express validator and express-async-errors

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,8 +1,30 @@
 import express from 'express';
+import 'express-async-errors';
+import { json } from 'body-parser';
+
+import { currentUserRouter } from './routes/current-user';
+import { signinRouter } from './routes/signin';
+import { signoutRouter } from './routes/signout';
+import { signupRouter } from './routes/signup';
+import { errorHandler } from './middlewares/error-handler';
+import { NotFoundError } from './errors/not-found-error';
 
 const app = express();
-app.use(express.json());
+app.use(json());
 
-app.listen(3000, ()=>{
-    console.log("App started on 3000!")
-})
+app.use(currentUserRouter);
+app.use(signinRouter);
+app.use(signoutRouter);
+app.use(signupRouter);
+
+// we can throw errors from async functions because of express-async-errors package
+app.all('*', async (req, res) => {
+  throw new NotFoundError();
+});
+// it's going to catch all thrown errors because
+// of the way express works
+app.use(errorHandler);
+
+app.listen(3000, () => {
+  console.log('Listening on port 3000!!!!!!!!');
+});
