051ba7fcab0be5db458b8e67210abb6e834f477f
Rares Jianu <Rares.Jianu@endava.com>
2022-02-03 11:43:21 +0200

    Add validation request middleware and signout, signin

--- auth/src/routes/signout.ts
+++ auth/src/routes/signout.ts
@@ -3,7 +3,11 @@ import express from "express";
 const router = express.Router();
 
 router.post("/api/users/signout", (req, res) => {
-  res.send("Hi there!");
+  // destroy the session
+  // and the cookies related to it
+  req.session = null;
+
+  res.send({});
 });
 
 export { router as signoutRouter };
