101c5bd43090b8dc3d1acc6dd6f16b3f9a6a98fd
Rares Jianu <Rares.Jianu@endava.com>
2022-02-07 14:11:02 +0200

    Add tickets service

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -8,9 +8,12 @@ const start = async () => {
     if (!process.env.JWT_KEY) {
         throw new Error("JWT_KEY must be defined");
     }
-    console.log("JWT SECRET ->", process.env.JWT_KEY)
+    if(!process.env.MONGO_URI) {
+        throw new Error("No mongo uri provided");
+    }
+
     try {
-        await mongoose.connect("mongodb://auth-mongo-srv:27017/auth");
+        await mongoose.connect(process.env.MONGO_URI);
         console.log("Connected to mongo db");
     } catch (err) {
         console.log(err);
